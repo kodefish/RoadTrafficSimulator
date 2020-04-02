bf72e951c2b9d81936b4f533f4eed603208298b3
Danil Ivanov <divanov@student.ethz.ch>
2020-04-02 16:44:10 +0200

    basic 2d primitives viewer in monogame

---
+++ RoadTrafficSimulator/GraphicPrimitives/Primitives2D.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RoadTrafficSimulator.GraphicPrimitives
+{
+    // Basic C# code to render pixels, lines and rectangles in 2D
+    // adaptded from: Source: https://www.dreamincode.net/forums/topic/234298-c%23-xna-2d-primitives-pixel-line-rectangle-etc/
+    class Primitives2D
+    {
+        Texture2D pixelTexture;         // 1x1 white texture, think of it as a paint brush
+        GraphicsDevice graphicsDevice;  // Used to draw on the screen
+        SpriteBatch spriteBatch;        // Spritebatch used
+
+        public void LoadContent(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
+        {
+            this.graphicsDevice = graphicsDevice;
+            this.spriteBatch = spriteBatch;
+
+            pixelTexture = new Texture2D(graphicsDevice, 1, 1);
+            pixelTexture.SetData(new Color[] { Color.White });
+        }
+
+        // Draws a pixel of color col at (x, y)
+        public void DrawPixel(float x, float y, Color color)
+        {
+            spriteBatch.Draw(pixelTexture, new Vector2(x, y), color);
+        }
+
+        // Draw line from (x1, y1) to (x2, y2)
+        public void DrawLine(float x1, float y1, float x2, float y2, Color color, int thickness = 1)
+        {
+            // Get actualy points
+            Vector2 p1 = new Vector2(x1, y1);
+            Vector2 p2 = new Vector2(x2, y2);
+
+            // Comput length and angle
+            float length = Vector2.Distance(p1, p2);
+            float angle = (float) Math.Atan2(p2.Y - p1.Y, p2.X - p1.X);
+
+            // Construct and draw corresponding line
+            Rectangle rectangle = new Rectangle((int) x1, (int) y1, (int) length, thickness);
+            spriteBatch.Draw(pixelTexture, rectangle, null, color, angle, Vector2.Zero, SpriteEffects.None, 0.0f);
+        }
+
+        // Draw rectangle centered at (x, y), of of size widthxheight, of a certain color
+        public void DrawRectangle(float x, float y, float width, float height, Color color, bool filled = true)
+        {
+            if (filled)
+            {
+                spriteBatch.Draw(pixelTexture, new Rectangle((int) x, (int) y, (int) width, (int) height), color);
+            }
+            else
+            {
+                DrawLine(x, y, x + width, y, color);                    // Top
+                DrawLine(x + width, y, x + width, y + height, color);   // Right
+                DrawLine(x + width, y + height, x, y + height, color);  // Bottom
+                DrawLine(x, y + height, x, y, color);           // Left
+            }
+        }
+    }
+}
