ba324dc87aa74f5e3f66d741e12a9b88bb63405c
Danil Ivanov <divanov@student.ethz.ch>
2020-04-03 16:18:41 +0200

    added bezier curve skeleton

--- RoadTrafficSimulator/DataStructures/BezierCurve.cs
+++ RoadTrafficSimulator/DataStructures/BezierCurve.cs
@@ -8,5 +8,19 @@ namespace RoadTrafficSimulator.DataStructures
 {
     class BezierCurve
     {
+        private readonly Vector2 supportPoint1, supportPoint2, supportPoint3, supportPoint4;
+
+        public BezierCurve(Vector2 supportPoint1,Vector2 supportPoint2,Vector2 supportPoint3,Vector2 supportPoint4)
+        {
+            this.supportPoint1 = supportPoint1;
+            this.supportPoint2 = supportPoint2;
+            this.supportPoint3 = supportPoint3;
+            this.supportPoint4 = supportPoint4;
+        }
+
+        public Vector2 GetPosition(float step)
+        {
+            throw new NotImplementedException(); 
+        }
     }
 }
