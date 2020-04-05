820b912e9c7f5e77a6f4e0aaf5dcaf84b8f95d46
Danil Ivanov <divanov@student.ethz.ch>
2020-04-05 15:04:26 +0200

    added a rigid body class, along with a car class skeleton using rigid body dynamics

---
+++ RoadTrafficSimulator/Simulator/Physics/RigidBody.cs
@@ -0,0 +1,68 @@
+﻿using RoadTrafficSimulator.DataStructures;
+
+namespace RoadTrafficSimulator.Simulator.Physics
+{
+    /// <summary>
+    /// Rigid body with a position, velocity, force and mass. Can apply some force on it to move it in some desired direction
+    /// </summary>
+    abstract class RigidBody
+    {
+        public float Mass { get; private set; }                         // In [kg]
+        public Vector2 Positon { get; private set; }                    // In [meters]
+        public Vector2 Velocity { get; private set; }                   // In [meters/second]
+        public Vector2 Force { get; private set; }                      // In [Newtons]
+        public Vector2 Acceleration { get { return Force / Mass; } }    // In [meters/second^2]
+
+        /// <summary>
+        /// Create a rigid body with mass, initial position, initial velocity, and initial force
+        /// </summary>
+        /// <param name="mass">Mass of the rigid body</param>
+        /// <param name="position">Initial position of the rigid body</param>
+        /// <param name="velocity">Initial velocity of the rigid body</param>
+        /// <param name="force">Inital force applied to the rigid body</param>
+        public RigidBody(float mass, Vector2 position, Vector2 velocity, Vector2 force)
+        {
+            Mass = mass;
+            Positon = position;
+            Velocity = velocity;
+            Force = force;
+        }
+
+        /// <summary>
+        /// Create a rigid body with mass, initial position, and initial velocity
+        /// </summary>
+        /// <param name="mass">Mass of the rigid body</param>
+        /// <param name="position">Initial position of the rigid body</param>
+        /// <param name="velocity">Initial velocity of the rigid body</param>
+        public RigidBody(float mass, Vector2 position, Vector2 velocity) : this(mass, position, velocity, new Vector2()) { }
+
+        /// <summary>
+        /// Create a rigid body with mass and initial position
+        /// </summary>
+        /// <param name="mass">Mass of the rigid body</param>
+        /// <param name="position">Initial position of the rigid body</param>
+        public RigidBody(float mass, Vector2 position) : this(mass, position, new Vector2(), new Vector2()) { }
+
+        /// <summary>
+        /// Apply an additional force to the rigid body
+        /// </summary>
+        /// <param name="force">Force to apply, in Newtons</param>
+        public void ApplyForce(Vector2 force) => Force += force;
+
+        /// <summary>
+        /// Apply the forces to the rigid body, update position and velocity given current force
+        /// </summary>
+        /// <param name="deltaTime">Time step over which to apply the force</param>
+        public void IntegrateForces(float deltaTime)
+        {
+            float deltaTime2 = deltaTime * deltaTime;
+
+            // p = p + v*t + 0.5 * a * t^2 (basic physics)
+            Positon = Positon + Velocity * deltaTime + Acceleration * 0.5f * deltaTime2;
+
+            // v = v + a * t
+            Velocity = Velocity + Acceleration * deltaTime;
+        }
+
+    }
+}
---
+++ RoadTrafficSimulator/Simulator/WorldEntities/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RoadTrafficSimulator.DataStructures;
+using RoadTrafficSimulator.Simulator.Physics;
+
+namespace RoadTrafficSimulator.Simulator.WorldEntities
+{
+    class Car : RigidBody
+    {
+        // TODO Add trajectory, some waypoints, etc
+
+        /// <summary>
+        /// Creates a car with a mass and an initial position
+        /// </summary>
+        /// <param name="mass"></param>
+        /// <param name="position"></param>
+        public Car(float mass, Vector2 position) : base(mass, position)
+        {
+
+        }
+
+        /// <summary>
+        /// Compute update using the Intelligent Driver Model for car-following model and MOBIL lane change criterion
+        /// </summary>
+        /// <param name="deltaTime">Time since last update</param>
+        public void Update(float deltaTime)
+        {
+            // Update forces acting on the car
+            Vector2 targetAcceleration = new Vector2(); // TODO
+            Vector2 deltaAcceleration = targetAcceleration - Acceleration;
+            Vector2 deltaForce = deltaAcceleration * Mass;
+            ApplyForce(deltaForce);
+        }
+}
