5e966d8c7ec5d7b71a7bb78dc575b710a0fd9bf8
Danil Ivanov <divanov@student.ethz.ch>
2020-04-09 21:59:39 +0200

    6 intersection grid

--- RoadTrafficSimulator/RoadTrafficSimulator.cs
+++ RoadTrafficSimulator/RoadTrafficSimulator.cs
@@ -66,7 +66,7 @@ namespace RoadTrafficSimulator
 
             spriteBatch.Begin();
 
-            wManager.Draw();
+            wManager.Draw(gameTime);
 
             spriteBatch.End();
         }
--- RoadTrafficSimulator/WorldManager.cs
+++ RoadTrafficSimulator/WorldManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -32,7 +33,7 @@ namespace RoadTrafficSimulator
             world = new SimulatorWorld();
             rtsRenderer = new RTSRenderer();
 
-            NUM_CARS = 50;
+            NUM_CARS = 100;
             rng = new Random(NUM_CARS);
             lastCarAddedTime = 0;
             carColor = new Dictionary<Car, Color>();
@@ -40,12 +41,12 @@ namespace RoadTrafficSimulator
 
         public void Initialize()
         {
-            GenerateSquare();
+            GenerateGrid();
             // GenerateHorizontalStrip();
             // GenerateVerticalStrip();
         }
 
-        private void GenerateSquare()
+        private void GenerateGrid()
         {
             float scale = 10;
             rtsRenderer.Scale = scale;
@@ -59,28 +60,42 @@ namespace RoadTrafficSimulator
             Vector2 posIntersection2 = new Vector2(0.25f * displayWidth, 0.75f * displayHeight);
             FourWayIntersection intersection2 = new FourWayIntersection(posIntersection2);
 
-            Vector2 posIntersection3 = new Vector2(0.75f * displayWidth, 0.25f * displayHeight);
+            Vector2 posIntersection3 = new Vector2(0.5f * displayWidth, 0.25f * displayHeight);
             FourWayIntersection intersection3 = new FourWayIntersection(posIntersection3);
 
-            Vector2 posIntersection4 = new Vector2(0.75f * displayWidth, 0.75f * displayHeight);
+            Vector2 posIntersection4 = new Vector2(0.5f * displayWidth, 0.75f * displayHeight);
             FourWayIntersection intersection4 = new FourWayIntersection(posIntersection4);
+
+            Vector2 posIntersection5 = new Vector2(0.75f * displayWidth, 0.25f * displayHeight);
+            FourWayIntersection intersection5 = new FourWayIntersection(posIntersection5);
+
+            Vector2 posIntersection6 = new Vector2(0.75f * displayWidth, 0.75f * displayHeight);
+            FourWayIntersection intersection6 = new FourWayIntersection(posIntersection6);
 
 
             Road road12 = new Road(ref intersection1, ref intersection2, 1, 1, RoadOrientation.Vertical, 120);
             Road road13 = new Road(ref intersection1, ref intersection3, 1, 1, RoadOrientation.Horizontal, 120);
             Road road24 = new Road(ref intersection2, ref intersection4, 1, 1, RoadOrientation.Horizontal, 120);
             Road road34 = new Road(ref intersection3, ref intersection4, 1, 1, RoadOrientation.Vertical, 120);
+            Road road35 = new Road(ref intersection3, ref intersection5, 1, 1, RoadOrientation.Horizontal, 120);
+            Road road46 = new Road(ref intersection4, ref intersection6, 1, 1, RoadOrientation.Horizontal, 120);
+            Road road56 = new Road(ref intersection5, ref intersection6, 1, 1, RoadOrientation.Vertical, 120);
 
             // Add the stuff
             world.AddIntersection(intersection1);
             world.AddIntersection(intersection2);
             world.AddIntersection(intersection3);
             world.AddIntersection(intersection4);
+            world.AddIntersection(intersection5);
+            world.AddIntersection(intersection6);
 
             world.AddRoad(road12);
             world.AddRoad(road13);
             world.AddRoad(road24);
             world.AddRoad(road34);
+            world.AddRoad(road35);
+            world.AddRoad(road46);
+            world.AddRoad(road56);
         }
 
         private void GenerateHorizontalStrip()
@@ -129,9 +144,12 @@ namespace RoadTrafficSimulator
 
         private void AddRandomCar(Random rng)
         {
+            // Try adding a car until one succeeds (adding may fail if the selected lane does not have enough free space)
+            // Limit the number of tries, because otherwise we may loop forever (no lane may have enough space)
             bool added = false;
+            int numTries = 0;
 
-            while (!added)
+            while (!added && numTries++ < 2 * world.Roads.Count)
             {
                 Road randomRoad = world.Roads[rng.Next(0, world.Roads.Count)];
                 Lane[] lanes = rng.Next() % 2 == 0 ? randomRoad.NorthBoundLanes : randomRoad.SouthBoundLanes;
@@ -166,12 +184,20 @@ namespace RoadTrafficSimulator
             rtsRenderer.LoadContent(graphicsDevice, spriteBatch, content);
         }
 
-        public void Draw()
+        public void Draw(GameTime gameTime)
         {
             // Draw the stuff
             foreach (FourWayIntersection intersection in world.Intersections) rtsRenderer.DrawIntersection(intersection);
             foreach (Road road in world.Roads) rtsRenderer.DrawRoad(road);
             foreach (Car car in world.Cars) rtsRenderer.DrawCar(car, carColor[car]);
+
+            // Print stats to console
+            Debug.WriteLine("World Stats:");
+            Debug.WriteLine("Num active cars: {0}", world.Cars.Count);
+            Debug.WriteLine("Num roads: {0}", world.Roads.Count);
+            Debug.WriteLine("Num intersections: {0}", world.Intersections.Count);
+            Debug.WriteLine("FPS: {0}", 1 / (float)gameTime.ElapsedGameTime.TotalSeconds);
+
         }
 
         public void Update(GameTime gameTime)
