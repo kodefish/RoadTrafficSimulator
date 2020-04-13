9cf638f72132ab3a889c079bec658ab3dea14c28
Danil Ivanov <divanov@student.ethz.ch>
2020-04-13 15:22:59 +0200

    cleaned up renderer

--- RoadTrafficSimulator/Graphics/RTSRenderer.cs
+++ RoadTrafficSimulator/Graphics/RTSRenderer.cs
@@ -36,7 +36,7 @@ namespace RoadTrafficSimulator.Graphics
             carTexture = content.Load<Texture2D>("Vehicles/Car");
         }
 
-        public void DrawRectange(Rectangle rectangle, Color c)
+        public void DrawScaledRectange(Rectangle rectangle, Color c)
         {
             Vector2 location = rectangle.Origin * Scale;
             Rectangle sourceRectangle = new Rectangle(new Vector2(0, 0), rectangle.Width, rectangle.Length);
@@ -46,71 +46,35 @@ namespace RoadTrafficSimulator.Graphics
             dRenderer.Draw(sourceRectangle, c, location, origin, scale, rectangle.Angle);
         }
 
-        public void DrawSegment(Segment s, Color c, float thickness = 1)
+        public void DrawScaledSegment(Segment s, Color c, float thickness = 1)
         {
             Segment scaledSegment = new Segment(s.Source * Scale, s.Target * Scale);
             dRenderer.Draw(scaledSegment, c, thickness);
         }
 
-        public void DrawPath(Path p, Color c)
-        {
-            foreach (Segment s in p.Segments) DrawSegment(s, c);
-        }
-
-
         public void DrawIntersection(FourWayIntersection intersection)
         {
-            DrawRectange(intersection.GetGeometricalFigure(), intersectionColor);
-        }
-
-        public void DrawNormal(Segment s, Color c, float thickness = 1)
-        {
-            Segment normalVec = new Segment(
-                s.Midpoint, 
-                s.Midpoint + s.Vector.Normal);
-
-            DrawSegment(normalVec, c, thickness);
+            DrawScaledRectange(intersection.GetGeometricalFigure(), intersectionColor);
         }
 
         public void DrawRoad(Road road)
         {
-            DrawRectange(road.GetGeometricalFigure(), roadColor);
+            DrawScaledRectange(road.GetGeometricalFigure(), roadColor);
             foreach (Lane l in road.InLanes) DrawLane(l, colors[l.LaneIdx % colors.Length]);
             foreach (Lane l in road.OutLanes) DrawLane(l, colors[l.LaneIdx % colors.Length]);
-            DrawSegment(road.RoadMidline, Color.Yellow);
+            DrawScaledSegment(road.RoadMidline, Color.Yellow);
         }
 
         public void DrawLane(Lane l, Color c)
         {
-            // Draw the midline
-            foreach (Segment s in l.Path.Segments) DrawSegment(s, c, 1);
-
             // Draw arrival points
             dRenderer.DrawPoint(l.Path.PathStart * Scale, Color.Green, 5);
             dRenderer.DrawPoint(l.Path.PathEnd * Scale, Color.Red, 5);
-
-            /*
-            // Draw free space
-            Segment freeLaneSpace = new Segment(
-                l.Path.PathStart * Scale, 
-                (l.Path.PathStart + l.Path.TangentOfProjectedPosition(l.Path.PathStart) * l.FreeLaneSpace()) * Scale);
-            dRenderer.Draw(freeLaneSpace, Color.Pink);
-
-            // Draw the vector to the closest corner of car in front
-            List<Car> cars = l.Cars;
-            for (int i = 0; i < l.Cars.Count - 1; i++)
-            {
-                Car c1 = cars[i]; Car c2 = cars[i + 1];
-                Vector2 p1 = c2.GetGeometricalFigure().ClosestVertex(c1.Position);
-                Vector2 p2 = c1.GetGeometricalFigure().ClosestVertex(p1);
-                dRenderer.Draw(new Segment(p1 * Scale, p2 * Scale), Color.Cyan);
-            }
-            */
         }
 
         public void DrawCar(Car c, Color color)
         {
-            DrawRectange(c.GetGeometricalFigure(), color);
+            DrawScaledRectange(c.GetGeometricalFigure(), color);
         }
     }
 }
