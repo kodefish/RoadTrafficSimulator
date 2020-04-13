bf42dc6e124c93245338ecc4e3f543f57b871394
Danil Ivanov <divanov@student.ethz.ch>
2020-04-13 12:25:23 +0200

    added neighbor test setup

--- RoadTrafficSimulator/WorldManager.cs
+++ RoadTrafficSimulator/WorldManager.cs
@@ -7,8 +7,10 @@ using Microsoft.Xna.Framework.Content;
 using RoadTrafficSimulator.Simulator;
 using RoadTrafficSimulator.Simulator.WorldEntities;
 using RoadTrafficSimulator.Simulator.DrivingLogic;
+using RoadTrafficSimulator.Simulator.DrivingLogic.FiniteStateMachine;
 using RoadTrafficSimulator.Graphics;
 using Vector2 = RoadTrafficSimulator.Simulator.DataStructures.LinAlg.Vector2;
+using RoadTrafficSimulator.Simulator.DataStructures.Geometry;
 
 namespace RoadTrafficSimulator
 {
@@ -42,8 +44,65 @@ namespace RoadTrafficSimulator
         public void Initialize()
         {
             // GenerateGrid();
-            GenerateHorizontalStrip();
-            // GenerateVerticalStrip();
+            // GenerateHorizontalStrip();
+            TestNeihboss();
+        }
+
+        private void TestNeihboss()
+        {
+            float scale = 20;
+            rtsRenderer.Scale = scale;
+            float displayWidth = game.GraphicsDevice.DisplayMode.Width / scale;
+            float displayHeight = game.GraphicsDevice.DisplayMode.Height / scale;
+
+            Vector2 start = new Vector2(displayWidth / 2 - 9, displayHeight / 2);
+            Vector2 hor = Vector2.UnitX;
+            FourWayIntersection i1 = new FourWayIntersection(start);
+            FourWayIntersection i2 = new FourWayIntersection(start + hor * 18);
+            Road r = new Road(ref i1, ref i2, 0, 3, 120);
+            world.AddIntersection(i1);
+            world.AddIntersection(i2);
+            world.AddRoad(r);
+
+            CarParams carParams = new CarParams(                            
+                mass : 500,
+                carWidth : 2,
+                carLength : 2,
+                maxSpeed : 120,
+                maxAccleration : 1.3f,
+                brakingDeceleration: 3f,
+                politenessFactor: 0.0f);
+            
+            Lane laneUp = r.OutLanes[2];
+            Car cA = new Car(carParams, laneUp, 6 / laneUp.Path.Length);
+            Car cB = new Car(carParams, laneUp, 12 / laneUp.Path.Length);
+
+            Lane laneMiddle = r.OutLanes[1];
+            Car c1 = new Car(carParams, laneMiddle, 3 / laneUp.Path.Length);
+            Car c2 = new Car(carParams, laneMiddle, 9 / laneUp.Path.Length);
+            Car c3 = new Car(carParams, laneMiddle, 15 / laneUp.Path.Length);
+
+
+            Lane laneDown = r.OutLanes[0];
+            Car cI = new Car(carParams, laneDown, 6 / laneUp.Path.Length);
+            Car cJ = new Car(carParams, laneDown, 12 / laneUp.Path.Length);
+
+            world.AddCar(cA);
+            world.AddCar(cB);
+            world.AddCar(c1);
+            world.AddCar(c2);
+            world.AddCar(c3);
+            world.AddCar(cI);
+            world.AddCar(cJ);
+
+
+            carColor.Add(cA, colors[rng.Next(colors.Length)]);
+            carColor.Add(cB, colors[rng.Next(colors.Length)]);
+            // carColor.Add(c1, colors[rng.Next(colors.Length)]);
+            carColor.Add(c2, colors[rng.Next(colors.Length)]);
+            // carColor.Add(c3, colors[rng.Next(colors.Length)]);
+            // carColor.Add(cI, colors[rng.Next(colors.Length)]);
+            // carColor.Add(cJ, colors[rng.Next(colors.Length)]);
         }
 
         private void GenerateGrid()
@@ -113,29 +172,7 @@ namespace RoadTrafficSimulator
             Vector2 posIntersection2 = new Vector2(displayWidth - padding, displayHeight / 2);
             FourWayIntersection intersection2 = new FourWayIntersection(posIntersection2);
 
-            Road road = new Road(ref intersection1, ref intersection2, 2, 2, 30);
-
-            world.AddIntersection(intersection1);
-            world.AddIntersection(intersection2);
-            world.AddRoad(road);
-        }
-
-        private void GenerateVerticalStrip()
-        {
-            float scale = 10;
-            rtsRenderer.Scale = scale;
-            float displayWidth = game.GraphicsDevice.DisplayMode.Width / scale;
-            float displayHeight = game.GraphicsDevice.DisplayMode.Height / scale;
-
-            float padding = displayWidth / 4;
-
-            Vector2 posIntersection1 = new Vector2(displayWidth / 2, 10);
-            FourWayIntersection intersection1 = new FourWayIntersection(posIntersection1);
-
-            Vector2 posIntersection2 = new Vector2(displayWidth / 2, displayHeight - 10);
-            FourWayIntersection intersection2 = new FourWayIntersection(posIntersection2);
-
-            Road road = new Road(ref intersection2, ref intersection1, 0, 1, 30);
+            Road road = new Road(ref intersection1, ref intersection2, 0, 2, 30);
 
             world.AddIntersection(intersection1);
             world.AddIntersection(intersection2);
@@ -183,7 +220,7 @@ namespace RoadTrafficSimulator
                         );
                     }
 
-                    Lane randomLane = lanes[rng.Next(0, lanes.Length)];
+                    Lane randomLane = lanes[0];//rng.Next(0, lanes.Length)];
                     if (randomLane.FreeLaneSpace() > carParams.CarLength + IntelligentDriverModel.MIN_BUMPER_TO_BUMPER_DISTANCE)
                     {
                         Car car = new Car(carParams, randomLane, (float)rng.NextDouble());
@@ -211,6 +248,31 @@ namespace RoadTrafficSimulator
             foreach (Road road in world.Roads) rtsRenderer.DrawRoad(road);
             foreach (Car car in world.Cars) rtsRenderer.DrawCar(car, carColor[car]);
 
+            // Draw neighbor info for each car in the middle lane
+            Road r = world.Roads[0];
+            Lane observedLane = r.OutLanes[2];
+            for (int i = 0; i < observedLane.Cars.Count; i++)
+            {
+                Car c = observedLane.Cars[i];
+                // Get neighbor info
+                foreach (Lane l in observedLane.NeighboringLanes)
+                {
+                    VehicleNeighbors vNeighbors = l.VehicleNeighbors(c);
+                    if (vNeighbors.VehicleBack != null) rtsRenderer.DrawSegment(new Segment(c.Position, vNeighbors.VehicleBack.Position), Color.Red);
+                    if (vNeighbors.VehicleFront != null) rtsRenderer.DrawSegment(new Segment(c.Position, vNeighbors.VehicleFront.Position), Color.Green);
+                }
+
+                // Get lane info
+                LeaderCarInfo leaderCarInfo;
+                Vector2 frontBumper = c.Position + c.Direction * c.CarLength / 2;
+                if (i < observedLane.Cars.Count - 1)
+                    leaderCarInfo = observedLane.ComputeLeaderCarInfo(c, observedLane.Cars[i + 1]);
+                else
+                    leaderCarInfo = observedLane.ComputeLeaderCarInfo(c);
+
+                rtsRenderer.DrawSegment(new Segment(frontBumper, frontBumper + c.Direction * leaderCarInfo.DistToNextCar), Color.Black);
+            }
+
             // Print stats to console
             /*
             Debug.WriteLine("World Stats:");
@@ -223,12 +285,14 @@ namespace RoadTrafficSimulator
 
         public void Update(GameTime gameTime)
         {
+            /*
             // Add a car every 3 seconds
             if (world.Cars.Count < NUM_CARS && gameTime.TotalGameTime.TotalSeconds - lastCarAddedTime > CAR_ADDITION_WAIT_TIME)
             {
                 AddRandomCar(rng);
                 lastCarAddedTime = gameTime.TotalGameTime.TotalSeconds;
             }
+            */
             //  Update the stuff
             world.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
         }
