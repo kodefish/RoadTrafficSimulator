574a049df82119ae08832991918d94bcc066886e
Danil Ivanov <divanov@student.ethz.ch>
2020-04-19 15:40:45 +0200

    interface documentation additions

--- RoadTrafficSimulator/Simulator/Interfaces/IRTSGeometry.cs
+++ RoadTrafficSimulator/Simulator/Interfaces/IRTSGeometry.cs
@@ -2,8 +2,15 @@
 
 namespace RoadTrafficSimulator.Simulator.Interfaces
 {
+    /// <summary>
+    /// Interface indicates that an object has some geometrical representation
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     interface IRTSGeometry<T> where T : GeometricalFigure
     {
+        /// <summary>
+        /// Geometrical representation of the object
+        /// </summary>
         T GetGeometricalFigure();
     }
 }
