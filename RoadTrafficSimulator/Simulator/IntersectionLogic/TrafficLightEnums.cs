582ae2b7c90fd360f9473124f3e86686afae4901
Danil Ivanov <divanov@student.ethz.ch>
2020-04-19 15:52:48 +0200

    intersection logic/traffic light fsm documentation

--- RoadTrafficSimulator/Simulator/IntersectionLogic/TrafficLightEnums.cs
+++ RoadTrafficSimulator/Simulator/IntersectionLogic/TrafficLightEnums.cs
@@ -3,12 +3,26 @@ using RoadTrafficSimulator.Simulator.DataStructures.LinAlg;
 
 namespace RoadTrafficSimulator.Simulator.IntersectionLogic
 {
+    /// <summary>
+    /// Represents a cardinal direction (north, east, south, west)
+    /// </summary>
     enum CardinalDirection { NORTH, EAST, SOUTH, WEST }
+
+    /// <summary>
+    /// Possible turn directions (front, right, left)
+    /// </summary>
     enum TurnDirection { FRONT, RIGHT, LEFT }
+
+    /// <summary>
+    /// Intersection traffic flow (vertical/horizontal -> NS/EW and front + right / left)
+    /// </summary>
     enum IntersectionFlowState { NS_FR, NS_L, EW_FR, EW_L }
 
     class TrafficEnum
     {
+        /// <summary>
+        /// Compute cardinal direction of a vector
+        /// </summary>
         public static CardinalDirection GetCardinalDirection(Vector2 a)
         {
             float xDot = Vector2.Dot(a, Vector2.UnitX);
@@ -21,6 +35,11 @@ namespace RoadTrafficSimulator.Simulator.IntersectionLogic
             throw new ArgumentException("Vector must be aligned to x or y axis");
         }
 
+        /// <summary>
+        /// Compute the relative turn direction between two vectors
+        /// </summary>
+        /// <param name="inDir">Direction coming into the turn</param>
+        /// <param name="outDir">Direction coming out of the turn</param>
         public static TurnDirection GetTurnDirection(Vector2 inDir, Vector2 outDir)
         {
             float turnSign = inDir.X * outDir.Y - outDir.X * inDir.Y;
@@ -28,6 +47,13 @@ namespace RoadTrafficSimulator.Simulator.IntersectionLogic
             else if (turnSign < 0) return TurnDirection.LEFT;
             else return TurnDirection.FRONT;
         }
+
+        /// <summary>
+        /// Get intersection traffic flow state (corresponding light basically) 
+        /// based on incoming directoin and relative turn direction
+        /// </summary>
+        /// <param name="inDirection">Direction coming into the intersection</param>
+        /// <param name="turnDirection">Relative turn direction</param>
         public static IntersectionFlowState GetStateIdx(CardinalDirection inDirection, TurnDirection turnDirection)
         {
             // God I miss Scala and pattern matching...
--- RoadTrafficSimulator/Simulator/IntersectionLogic/TrafficLightFSM.cs
+++ RoadTrafficSimulator/Simulator/IntersectionLogic/TrafficLightFSM.cs
@@ -6,17 +6,58 @@ using System;
 
 namespace RoadTrafficSimulator.Simulator.IntersectionLogic
 {
+    /// <summary>
+    /// Light states. Go is green, wait_for_empty is yellow, 
+    /// and red is whenever the TrafficLightFSM state is not the current state.
+    /// So when the traffic light FSM is active, cars may go through
+    /// </summary>
     enum LightStates { GO, WAIT_FOR_EMPTY };
+
+    /// <summary>
+    /// Traffic light state. Represents a certain set of active lanes being active in an intersection.
+    /// The state monitors the possible states and waits for the intersection to be empty before
+    /// letting a new state take over.
+    /// </summary>
     class TrafficLightFSM
     {
+        /// <summary>
+        /// Intersection to control
+        /// </summary>
         private readonly FourWayIntersection intersection;
+
+        /// <summary>
+        /// Current and next intersection flow state
+        /// </summary>
         private readonly IntersectionFlowState currentIntersectionFlowState, nextIntersectionFlowState;
+
+        /// <summary>
+        /// Mapping of entry segments (lane targets) to a list of possible next lanes
+        /// (some lanes may turn right and go forward)
+        /// </summary>
         public readonly Dictionary<int, List<Lane>> activeLanes;
+
+        /// <summary>
+        /// Duration, in seconds, of a green light
+        /// </summary>
         private readonly float greenLightTime;
 
+        /// <summary>
+        /// Time spent in green light state
+        /// </summary>
         private float counter;
+
+        /// <summary>
+        /// Current light state
+        /// </summary>
         private LightStates currentLightState;
 
+        /// <summary>
+        /// Creates a traffic light state
+        /// </summary>
+        /// <param name="intersection">Intersection to control</param>
+        /// <param name="currentIntersectionFlowState">Intersection flow state of this state</param>
+        /// <param name="nextIntersectionFlowState">Intersection flow state to transition to</param>
+        /// <param name="greenLightTime">Time to spend in green light</param>
         public TrafficLightFSM(
             FourWayIntersection intersection, 
             IntersectionFlowState currentIntersectionFlowState, 
@@ -30,12 +71,22 @@ namespace RoadTrafficSimulator.Simulator.IntersectionLogic
             activeLanes = new Dictionary<int, List<Lane>>();
         }
 
+        /// <summary>
+        /// Add an active lane. A lane target can thus be used to determine the next lane
+        /// </summary>
+        /// <param name="key">Source segment</param>
+        /// <param name="l">Lane starting at key</param>
         public void AddActiveLane(Segment key, Lane l) 
         {
             if (!activeLanes.ContainsKey(key.GetHashCode())) activeLanes.Add(key.GetHashCode(), new List<Lane>());
             activeLanes[key.GetHashCode()].Add(l);
         }
 
+        /// <summary>
+        /// Get a list of possible next lanes in given state starting at some segment
+        /// </summary>
+        /// <param name="s">Start segment</param>
+        /// <returns>Possible lanes starting at s</returns>
         public List<Lane> GetPossibleNextLanes(Segment s)
         {
             // Only let cars go through if the current light state is go and there are available lanes
@@ -43,10 +94,19 @@ namespace RoadTrafficSimulator.Simulator.IntersectionLogic
             else return new List<Lane>();
         }
 
+        /// <summary>
+        /// Clear the active lanes.
+        /// </summary>
         public void Clear() => activeLanes.Clear();
 
+        /// <summary>
+        /// Update the traffic light FSM.
+        /// </summary>
+        /// <param name="deltaTime">Time since the last update</param>
+        /// <returns></returns>
         public IntersectionFlowState Update(float deltaTime)
         {
+            // Update counter
             counter += deltaTime;
             if (currentLightState == LightStates.WAIT_FOR_EMPTY)
             {
@@ -62,6 +122,10 @@ namespace RoadTrafficSimulator.Simulator.IntersectionLogic
             return currentIntersectionFlowState;
         }
 
+        /// <summary>
+        /// Checks to see if all the currently active lanes are empty (no cars)
+        /// </summary>
+        /// <returns>True if all active lanes are empty</returns>
         private bool ActiveLanesEmpty()
         {
             bool isEmpty = true;
@@ -71,11 +135,18 @@ namespace RoadTrafficSimulator.Simulator.IntersectionLogic
             return isEmpty;
         }
 
+        /// <summary>
+        /// On Enter traffic light state. Reset the counter to zero and light to green
+        /// </summary>
         public void OnEnter()
         {
             counter = 0;
             currentLightState = LightStates.GO;
         }
+
+        /// <summary>
+        /// On Exit traffic light state. Reset light to green
+        /// </summary>
         public void OnExit()
         {
             currentLightState = LightStates.GO;
